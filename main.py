10023aea9119534bb467906e961c5b998473efd8
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-10-28 20:13:53 +0100

    Add a function which randomize variable names

---
+++ main.py
@@ -0,0 +1,35 @@
+import re
+import secrets
+import string
+from typing import Iterable
+
+VBA_PATH = "download_payload.vba"
+FIND_VAR_NAMES_REGEX = r"\s*Dim (\w+) "
+
+
+def read_file(path: str) -> str:
+    with open(path, "r") as f:
+        return f.read()
+
+
+def get_all_variables_iter(content: str) -> Iterable[str]:
+    var_names = re.finditer(FIND_VAR_NAMES_REGEX, content)
+    return map(lambda v: v.group(1), var_names)
+
+
+def get_random_variable_name(size: int) -> str:
+    return ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(size))
+
+
+def replace_var_name(content: str, var_name: str, new_name: str) -> str:
+    result = re.sub(r"(\s){}(\s)".format(var_name), r"\1{}\2".format(new_name), content)
+    return result
+
+
+if __name__ == "__main__":
+    vba_content = read_file(VBA_PATH)
+    for var in get_all_variables_iter(vba_content):
+        var_length = 7 + secrets.randbelow(16)
+        vba_content = replace_var_name(vba_content, var, get_random_variable_name(var_length))
+
+    print(replace_var_name(vba_content, "exec", "test"))
