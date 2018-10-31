1ee1d6679f4d97c4b5a1925da7fd48d88b66eb06
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-10-31 16:46:18 +0100

    Encode the key in base64 in doc var

--- obfuscator/modifier/strings.py
+++ obfuscator/modifier/strings.py
@@ -1,7 +1,9 @@
+import base64
 import logging
 import random
 import re
-from typing import Iterable
+import string
+from typing import Iterable, List
 
 from obfuscator.modifier.base import Modifier
 from obfuscator.msdocument import MSDocument
@@ -10,20 +12,22 @@ from obfuscator.util import get_random_string
 LOG = logging.getLogger(__name__)
 FIND_STRINGS_REGEX = r'"((?:[^"]|"")*)"'
 VBA_XOR_FUNCTION = """
-Private Function unxor(ciphertext As Variant, key As String)
+Private Function unxor(ciphertext As Variant, start As Integer)
     Dim cleartext As String
+    Dim key() As Byte
+    key = Base64Decode(ActiveDocument.Variables("{}"))
     cleartext = ""
     
     For i = LBound(ciphertext) To UBound(ciphertext)
-        keyChar = Mid(key, i + 1, 1)
-        cleartext = cleartext & Chr(Asc(keyChar) Xor ciphertext(i))
+        cleartext = cleartext & Chr(key(i+start) Xor ciphertext(i))
     Next
     unxor = cleartext
 
 End Function
 
-
 """
+with open("base64.vba") as f:
+    VBA_BASE64_FUNCTION = f.read()
 
 
 class SplitStrings(Modifier):
@@ -40,14 +44,38 @@ class SplitStrings(Modifier):
 
 class CryptStrings(Modifier):
     def run(self, doc: MSDocument) -> None:
+        LOG.debug('Generating document variable name.')
+        document_var = get_random_string(16)
+        cryptKey = []
+
         for str_found in _get_all_strings(doc.code):
-            key = get_random_string(len(str_found))
-            array = _to_vba_array(_xor_crypt(str_found, key))
-            unxor_eq = 'unxor({},"{}")'.format(array, key)
+            LOG.debug("Generating XOR key for '{}'.".format(str_found))
+            start = len(cryptKey)
+            key = _get_random_key(len(str_found))
+            cryptKey += key
+            LOG.debug("XOR key will be at [{}; {}].".format(start, len(key)))
+
+            ciphertext = _xor_crypt(str_found, key)
+            array = _to_vba_array(ciphertext)
+            LOG.debug("Encrypted string to VBA Array -> {}.".format(array))
+
+            unxor_eq = 'unxor({},{})'.format(array, start)
             doc.code = re.sub(re.escape('"{}"'.format(str_found)), unxor_eq, doc.code, 1)
+
             LOG.debug("Encrypted '{}'.".format(str_found))
 
-        doc.code = VBA_XOR_FUNCTION + doc.code
+        doc.code = VBA_BASE64_FUNCTION + VBA_XOR_FUNCTION.format(document_var) + doc.code
+
+        b64 = base64.b64encode(bytes(cryptKey)).decode()
+        LOG.info('''Paste this in your VBA editor to add the Document Variable:
+ActiveDocument.Variables.Add Name:="{}", Value:="{}"'''.format(document_var, b64))
+
+KEY_SPACE = string.ascii_letters + string.digits + string.punctuation + " "
+KEY_SPACE = KEY_SPACE.replace('"', '')
+
+
+def _get_random_key(n: int) -> List[int]:
+    return [random.randint(0, 255) for _ in range(n)]
 
 
 def _get_all_strings(content: str) -> Iterable[str]:
@@ -62,7 +90,6 @@ def _xor(t):
 
 def _xor_crypt(msg, key):
     msg = map(ord, msg)
-    key = map(ord, key)
     str_key = zip(msg, key)
     return map(_xor, str_key)
 
