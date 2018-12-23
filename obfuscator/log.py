5eb6813993f9171f4e119d2774479a64916876c1
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-12-23 10:56:19 +0100

    Set default logging level to INFO

--- obfuscator/log.py
+++ obfuscator/log.py
@@ -5,7 +5,7 @@ import sys
 def configure_logging():
     # create a stdout handler
     handler = logging.StreamHandler(sys.stderr)
-    handler.setLevel(logging.DEBUG)
+    handler.setLevel(logging.INFO)
 
     # create a logging format
     formatter = logging.Formatter('[%(levelname)s] %(message)s')
