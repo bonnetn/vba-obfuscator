e8727589b5b6955cf01b4e4d8946a9014afde0b2
elephacking <elephacking@users.noreply.github.com>
2021-08-03 19:52:45 +0800

    Update util.py
    Patching for the Keyword "Preserve"

--- obfuscator/util.py
+++ obfuscator/util.py
@@ -69,7 +69,7 @@ def get_variables_defined(code):
     :param code:
     :return:
     """
-    var_names = re.finditer("^\s*.*(?:Dim|Private|Public)[ ]((?:\w+(?:[ ]+As[ ]+\w+)?[, ]*)+)", code, flags=re.M)
+    var_names = re.finditer("^\s*.*(?:Dim(?: Preserve)?|Private|Public)[ ]((?:\w+(?:[ ]+As[ ]+\w+)?[, ]*)+)", code, flags=re.M)
     var_names = map(lambda x: x.group(1), var_names)
     var_names = map(extract_variables, var_names)
 
